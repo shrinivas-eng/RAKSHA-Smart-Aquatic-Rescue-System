6bff826b0ee67eaf8482c769cc631a0784f6cf0c
shrinivas-eng <shrinisiva2006@gmail.com>
2026-08-30 14:48:52 +0530

    Update rescuer_unit.ino

--- Sample_Code/rescuer_unit.ino
+++ Sample_Code/rescuer_unit.ino
@@ -1,1 +1,81 @@
+/*
+  RAKSHA - Smart Aquatic Rescue System
+  Sample Code: Rescue Unit
 
+  Demonstrates:
+  - Receiving a rescue alert
+  - Processing victim zone information
+  - Starting rescue operation
+  - Basic navigation workflow
+*/
+
+#define ALERT_LED_PIN 2
+
+String victimZone = "ZONE 3";
+String victimGPS = "GPS DATA AVAILABLE";
+
+void setup() {
+
+  Serial.begin(115200);
+
+  pinMode(ALERT_LED_PIN, OUTPUT);
+
+  Serial.println("--------------------------------");
+  Serial.println("RAKSHA Rescue Unit");
+  Serial.println("--------------------------------");
+
+  Serial.println("Rescue Unit Ready");
+  Serial.println("Waiting for Emergency Alert...");
+}
+
+void loop() {
+
+  // Sample emergency alert reception
+  receiveEmergencyAlert();
+
+  delay(5000);
+}
+
+
+void receiveEmergencyAlert() {
+
+  // Demonstration alert
+  bool rescueAlert = true;
+
+  if (rescueAlert) {
+
+    Serial.println("");
+    Serial.println("!!! RESCUE ALERT RECEIVED !!!");
+
+    digitalWrite(ALERT_LED_PIN, HIGH);
+
+    Serial.println("Victim Zone: " + victimZone);
+    Serial.println("GPS Information: " + victimGPS);
+
+    startRescueOperation();
+
+    digitalWrite(ALERT_LED_PIN, LOW);
+  }
+}
+
+
+void startRescueOperation() {
+
+  Serial.println("");
+  Serial.println("Starting Rescue Operation...");
+
+  Serial.println("1. Processing GPS location");
+  Serial.println("2. Preparing navigation system");
+  Serial.println("3. Activating rescue unit");
+  Serial.println("4. Moving towards victim location");
+  Serial.println("5. Initiating rescue procedure");
+
+  Serial.println("Rescue operation workflow completed.");
+
+  /*
+    Actual autonomous navigation,
+    motor control, camera processing,
+    red-light detection, and rescue
+    mechanism code are kept private.
+  */
+}
